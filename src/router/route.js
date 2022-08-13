ae9087aa7e3048866cfcade84574b14e3cfca183
liam2014 <>
2022-08-13 15:21:11 +0800

    feat: 加入2个全屏展示页面。

--- src/router/route.js
+++ src/router/route.js
@@ -14,6 +14,16 @@ const routes = [
     component: () => import('@/view/login/login'),
   },
   {
+    path: '/stock/detail',
+    name: 'stock-detail',
+    component: () => import('@/view/stock-data/detail'),
+  },
+  {
+    path: '/stock/cockpit',
+    name: 'stock-cockpit',
+    component: () => import('@/view/stock-data/cockpit'),
+  },
+  {
     redirect: '/404',
     path: '/:pathMatch(.*)',
   },
--- src/view/stock-data/data-index.vue
+++ src/view/stock-data/data-index.vue
@@ -1,1 +1,28 @@
-<template>index</template>
+<template>
+  <h1>Home</h1>
+  <nav>
+    <router-link to="/about">Home</router-link> | <router-link to="/stock/cockpit">驾驶舱</router-link> |
+    <router-link to="/stock/detail?id=sz001970">详情</router-link> |
+  </nav>
+  <div class="buttonBox">
+    <button @click="client_event('go', '/stock/detail?id=sz001970')">跳转页面</button>
+    <button @click="client_event('', {})">Change</button>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+
+export default {
+  setup() {
+    const router = useRouter()
+    const clientEvent = (ty = '', db = {}) => {
+      console.log(ty, db)
+      if (ty === 'go') {
+        router.push(db)
+      }
+    }
+    return { client_event: clientEvent }
+  },
+}
+</script>
